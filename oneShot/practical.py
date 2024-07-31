c93225e41f166f8107ad557229a78aeb28c922e8
BeanyZoldyck <86325894+BeanyZoldyck@users.noreply.github.com>
2024-07-31 03:54:48 -0400

    median lambda function

--- oneShot/practical.py
+++ oneShot/practical.py
@@ -1,3 +1,6 @@
 def sortDict(dic):
     '''sorts dictionary by values {'b':2,'a':1,'d':4,'c':3}->{'a':1,'b':2','c':3,d':4}'''
     return {{j:i for i, j in dic.items()}[i]:i for i in sorted(dic.values())}
+
+median = lambda li: li[len(li)//2] if (len(li)&1) else (li[len(li)//2]+li[len(li)//2 - 1]) / 2 #simply dont pass empty lists into this...
+    
